fa7bf0356aebb709dd73582666d5c4e72863eb39
dxvxlery <danicpuscas@gmail.com>
2023-08-13 01:35:36 +0300

    delete route

--- src/routes/iban.route.ts
+++ src/routes/iban.route.ts
@@ -6,8 +6,4 @@ import { IbanController } from "../controllers/iban.controller";
 
 router.get("/bankData/:iban", ValidateService.validateIban, IbanController.getByIban);
 
-router.get("/qwe", (req, res) => {
-  res.send("qwe1231");
-});
-
 export default router;
